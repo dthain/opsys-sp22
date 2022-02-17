b00f812657de0ec8f1cb78897de699de48583028
Douglas Thain <dthain@nd.edu>
2022-02-17 09:07:49 -0500

    Added semaphore examples.

---
+++ examples/sem-pc.c
@@ -0,0 +1,88 @@
+/* An example of using semaphores for the producer-consumer problem */
+
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define NBURGERS 4
+
+sem_t mutex;
+sem_t full;
+sem_t empty;
+
+int count = 0;
+int nthreads = 4;
+int sleeptime = 1000000; /* microseconds */
+
+void safe_insert()
+{
+  printf("thread %d wants to insert\n",(int)pthread_self());
+  sem_wait(&empty);
+  printf("thread %d inserting\n",(int)pthread_self());
+  sem_wait(&mutex);
+  count++;
+  printf("thread %d count: %d\n",(int)pthread_self(),count);
+  sem_post(&mutex);
+  sem_post(&full);
+}
+
+void safe_remove()
+{
+  printf("thread %d wants to remove\n",(int)pthread_self());
+  sem_wait(&full);
+  printf("thread %d removing\n",(int)pthread_self());
+  sem_wait(&mutex);
+  count--;
+  printf("thread %d count: %d\n",(int)pthread_self(),count);
+  sem_post(&mutex);
+  sem_post(&empty);
+}
+
+void * producer_thread( void * arg )
+{
+  while(1) {
+    usleep(rand()%sleeptime);
+    safe_insert();
+  }
+}
+
+void * consumer_thread( void * arg )
+{
+  while(1) {
+    usleep(rand()%sleeptime);
+    safe_remove();
+  }
+}
+
+int main( int argc, char *argv[] )
+{
+  pthread_t producer_tid[nthreads];
+  pthread_t consumer_tid[nthreads];
+  int i;
+
+  sem_init(&mutex,0,1);
+  sem_init(&full,0,0);
+  sem_init(&empty,0,NBURGERS);
+
+  for(i=0;i<nthreads;i++) {
+    pthread_create(&producer_tid[i],0,producer_thread,0);
+  }
+
+  for(i=0;i<nthreads;i++) {
+    pthread_create(&consumer_tid[i],0,consumer_thread,0);
+  }
+
+  for(i=0;i<nthreads;i++) {
+    void *result;
+    pthread_join(producer_tid[i],&result);
+    pthread_join(consumer_tid[i],&result);
+  }
+
+  printf("main: all threads done.\n");
+
+  return 0;
+}
+
---
+++ examples/sem-rw.c
@@ -0,0 +1,82 @@
+/* An example of using semaphores for the readers-writers problem */
+
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+sem_t mutex;
+sem_t writelock;
+int   readcount=0;
+int   value = 0;
+
+int nthreads = 4;
+int sleeptime = 1000000; /* microseconds */
+
+int safe_read()
+{
+  sem_wait(&mutex);
+  readcount++;
+  if(readcount==1) sem_wait(&writelock);
+  sem_post(&mutex);
+
+  int result = value;
+
+  sem_wait(&mutex);
+  readcount--;
+  if(readcount==0) sem_post(&writelock);
+  sem_post(&mutex);
+
+  return result;
+}
+
+void safe_write( int newvalue )
+{
+  sem_wait(&writelock);
+  value = newvalue;
+  sem_post(&writelock);
+}
+
+void * reader_writer_thread( void * arg )
+{
+  while(1) {
+    usleep(rand()%sleeptime);
+    if(rand()%2) {
+      printf("thread %d reading value...\n",(int)pthread_self());
+      int v = safe_read();
+      printf("thread %d read value %d\n",(int)pthread_self(),v);
+    } else {
+      int v = rand()%100;
+      printf("thread %d writing value %d\n",(int)pthread_self(),v);
+      safe_write(v);
+      printf("threed %d done writing %d\n",(int)pthread_self(),v);
+    }
+  }
+}
+
+int main( int argc, char *argv[] )
+{
+  pthread_t tid[nthreads];
+  int i;
+
+  sem_init(&mutex,0,1);
+  sem_init(&writelock,0,1);
+  readcount = 0;
+  value = 0;
+
+  for(i=0;i<nthreads;i++) {
+    pthread_create(&tid[i],0,reader_writer_thread,0);
+  }
+
+  for(i=0;i<nthreads;i++) {
+    void *result;
+    pthread_join(tid[i],&result);
+  }
+
+  printf("main: all threads done.\n");
+
+  return 0;
+}
+
